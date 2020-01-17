6f21d47ef11f341439db0b27154f723bd685cf17
dcy <sdf@a.com>
2020-01-17 16:45:14 +0800

    提交代码

--- src/main/java/com/example/demo/config/SpringSessionConfig.java
+++ src/main/java/com/example/demo/config/SpringSessionConfig.java
@@ -2,10 +2,13 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.FlushMode;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
+@EnableRedisHttpSession(flushMode = FlushMode.IMMEDIATE)
 public class SpringSessionConfig {
 
 
--- src/main/java/com/example/demo/config/security/SecurityConfig.java
+++ src/main/java/com/example/demo/config/security/SecurityConfig.java
@@ -3,15 +3,25 @@ package com.example.demo.config.security;
 import com.example.demo.mapper.AssignPrivilegeMapper;
 import com.example.demo.mapper.SysAccountMapper;
 import com.example.demo.mapper.SysAuthorityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -39,22 +49,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .formLogin()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/**/*.css").permitAll()
+//                .antMatchers("/**/*.js").permitAll()
+//                .antMatchers("/**/*.html").permitAll()
+//                .antMatchers("/favicon.ico").permitAll()
+//                .antMatchers("/").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .csrf().disable();
+
         http
                 .formLogin()
-                .loginPage("/login.html")
-                .loginProcessingUrl("/login")
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.getWriter().println(request.getSession().getId());
+                    }
+                })
                 .and()
                 .authorizeRequests()
-                .antMatchers("/**/*.css").permitAll()
-                .antMatchers("/**/*.js").permitAll()
-                .antMatchers("/**/*.html").permitAll()
-                .antMatchers("/favicon.ico").permitAll()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .csrf().disable();
         // 用户不能同时登陆，第二个无法登录
         http.sessionManagement().maximumSessions(1).expiredUrl("/login");
+
     }
+
+    @Autowired
+    private RedisIndexedSessionRepository redisIndexedSessionRepository;
 
 }
